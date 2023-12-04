70850379c6debf31421c8a26830006a892a54d5d
Brian Campbell <brian.k.campbell@outlook.com>
2023-12-04 00:45:58 -0500

    Day 4, Part 2

--- src/day04.py
+++ src/day04.py
@@ -22,23 +22,39 @@ def do_stuff():
     lines = data_file.read().split('\n')
 
     total_points = 0
+    card_copies = {}
 
-    for line in lines:
+    for i in range(len(lines)):
+        line = lines[i]
+
+        card_num = i + 1
+
         winning_nums = list(map(int, ((line.split(' | ')[0]).split(": ")[1].strip().replace('  ', ' ')).split(' ')))
-        print('wn')
-        print(winning_nums)
+        # print(winning_nums)
 
         elf_nums = list(map(int, ((line.split(' | ')[1]).strip().replace('  ', ' ')).split(' ')))
-        print('en')
-        print(elf_nums)
+        # print(elf_nums)
 
         ticket_hits = len([n for n in elf_nums if n in winning_nums])
         # print(ticket_hits)
 
         if ticket_hits > 0:
             total_points += pow(2, ticket_hits - 1)
+            copies_of_current_card = 1
+            if card_num in card_copies.keys():
+                copies_of_current_card += card_copies[card_num]
+
+            for j in range(ticket_hits):
+                card_copy_num = card_num + j + 1
+                if card_copy_num not in card_copies.keys():
+                    card_copies[card_copy_num] = copies_of_current_card
+                else:
+                    card_copies[card_copy_num] += copies_of_current_card
+
+    total_cards = len(lines) + sum(card_copies.values())
 
     print(f'Total Points: {total_points}\n############################\n')
+    print(f'Total Cards: {total_cards}\n############################\n')
 
 
 day_4()
