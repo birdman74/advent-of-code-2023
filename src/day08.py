f155532e2f1f2a6d6aa06fe8cce258cc4437c934
Brian Campbell <brian.k.campbell@outlook.com>
2023-12-08 00:48:49 -0500

    Day 8, Part 2

--- src/day08.py
+++ src/day08.py
@@ -1,4 +1,5 @@
 import os
+import numpy
 
 # INPUT_DIR = os.path.join('input', 'samples')
 INPUT_DIR = 'input'
@@ -11,6 +12,7 @@ INPUT_SOURCE_DIR = os.path.join(PROJECT_DIR, INPUT_DIR)
 
 LEFT = 0
 RIGHT = 1
+
 
 def day_08():
     do_stuff()
@@ -25,7 +27,7 @@ def do_stuff():
 
     directions = lines[0]
     directions_length = len(directions)
-    map = {}
+    lr_map = {}
 
     for i in range(2, len(lines)):
         line = lines[i]
@@ -34,18 +36,26 @@ def do_stuff():
         pieces = pieces[1][1:len(pieces[1]) - 1].split(', ')
         left = pieces[0]
         right = pieces[1]
-        map[source] = (left, right)
+        lr_map[source] = (left, right)
 
-    steps = 0
-    directions_index = 0
+    sources = [node for node in lr_map.keys() if node.endswith('A')]
+    z_distance_lcm = -1
 
-    source = 'AAA'
-    while source != 'ZZZ':
-        source = map[source][turn(directions[directions_index])]
-        directions_index = (directions_index + 1) % directions_length
-        steps += 1
+    for source in sources:
+        steps = 0
+        directions_index = 0
+        while not source.endswith('Z'):
+            source = lr_map[source][turn(directions[directions_index])]
 
-    print(f'Number of steps: {steps}\n############################\n')
+            directions_index = (directions_index + 1) % directions_length
+            steps += 1
+
+        if z_distance_lcm == -1:
+            z_distance_lcm = steps
+        else:
+            z_distance_lcm = numpy.lcm(z_distance_lcm, steps, dtype=object)
+
+    print(f'Number of steps: {z_distance_lcm}\n############################\n')
 
 
 def turn(direction):
