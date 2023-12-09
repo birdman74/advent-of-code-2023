28d759b00b25f3d868e3ba8c62e631c2fd34b0a9
Brian Campbell <brian.k.campbell@outlook.com>
2023-12-09 00:29:24 -0500

    Day 9, Part 1

---
+++ src/day09.py
@@ -0,0 +1,44 @@
+import os
+
+# INPUT_DIR = os.path.join('input', 'samples')
+INPUT_DIR = 'input'
+
+INPUT_FILE = 'day09.txt'
+
+MODULE_DIR = os.path.dirname(os.path.realpath(__file__))
+PROJECT_DIR = os.path.join(MODULE_DIR, '..')
+INPUT_SOURCE_DIR = os.path.join(PROJECT_DIR, INPUT_DIR)
+
+
+def day_09():
+    do_stuff()
+
+
+def do_stuff():
+    input_file = os.path.join(INPUT_SOURCE_DIR, INPUT_FILE)
+    print(f'Input file: {input_file}')
+
+    data_file = open(input_file)
+    lines = data_file.read().split('\n')
+
+    next_in_series_sum = 0
+
+    for line in lines:
+        nums = list(map(int, line.split(' ')))
+        next_in_series_sum += next_in_series(nums)
+
+    print(f'Next-in-Series sum: {next_in_series_sum}\n############################\n')
+
+
+def next_in_series(series):
+    new_series = []
+    for i in range(0, len(series) - 1):
+        new_series.append(series[i + 1] - series[i])
+
+    if len([x for x in new_series if x == 0]) == len(new_series):
+        return series[-1]
+    else:
+        return series[-1] + next_in_series(new_series)
+
+
+day_09()
