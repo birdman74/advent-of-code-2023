00dcc7f3d2d2c1ebcb6daaf359d679b178735755
Brian Campbell <brian.k.campbell@outlook.com>
2023-12-15 06:46:47 -0500

    Day 15, Part 2

--- src/day15.py
+++ src/day15.py
@@ -9,6 +9,9 @@ MODULE_DIR = os.path.dirname(os.path.realpath(__file__))
 PROJECT_DIR = os.path.join(MODULE_DIR, '..')
 INPUT_SOURCE_DIR = os.path.join(PROJECT_DIR, INPUT_DIR)
 
+DASH = '-'
+EQUALS = '='
+
 
 def day_15():
     do_stuff()
@@ -21,20 +24,58 @@ def do_stuff():
     data_file = open(input_file)
     lines = data_file.read().split('\n')
 
-    hash_total = 0
+    total_focusing_power = 0
 
     steps = lines[0].split(',')
 
+    # key = box num, val = {key = label, value = lens array index}
+    boxes = {}
+
     for step in steps:
-        i = 0
-        for c in step:
-            i += ord(c)
-            i *= 17
-            i %= 256
+        if step.find(EQUALS) > 0:
+            parts = step.split(EQUALS)
+            label = parts[0]
+            f_l = int(parts[1])
+            box_num = hash15(label)
+            if box_num in boxes.keys():
+                box = boxes[box_num]
+                box[label] = f_l
+            else:
+                boxes[box_num] = {label: f_l}
+        else:
+            label = step[0:len(step) - 1]
+            box_num = hash15(label)
+            if box_num in boxes.keys():
+                try:
+                    lenses = boxes[box_num]
 
-        hash_total += i
+                    del lenses[label]
+                except KeyError:
+                    pass
 
-    print(f'Hash total: {hash_total}\n############################\n')
+    for i in range(256):
+        try:
+            box = boxes[i]
+            focusing_power = 0
+            box_num = i + 1
+            for lens_index, label in enumerate(box):
+                focusing_power += (box_num * (lens_index + 1) * box[label])
+
+            total_focusing_power += focusing_power
+        except KeyError:
+            pass
+
+    print(f'Total focusing power: {total_focusing_power}\n############################\n')
+
+
+def hash15(label):
+    i = 0
+    for c in label:
+        i += ord(c)
+        i *= 17
+        i %= 256
+
+    return i
 
 
 day_15()
